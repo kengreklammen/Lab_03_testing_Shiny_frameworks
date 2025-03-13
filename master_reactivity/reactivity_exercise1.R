c5294a4109e89764086f4aeb6bf63305a0f625e1
kengreklammen <6339237+kengreklammen@users.noreply.github.com>
2025-03-13 14:45:31 +0100

    Commit with an almost-ready Shiny code.

--- master_reactivity/reactivity_exercise1.R
+++ master_reactivity/reactivity_exercise1.R
@@ -4,19 +4,39 @@ ui <- fluidPage(
   titlePanel("Iris database with reactive approach"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("item", "Choose an Item", choices = NULL), # to choose a species from iris$Species.
-      sliderInput("slider1", "Set value", min = 0, max = 100, value = 50), # to select a range of Sepal.Length values.
-      actionButton("action", "Action") # to filter the data
+      selectInput("select_species", "Choose a species", choices = c("setosa", "versicolor", "virginica")), # to choose a species from iris$Species.
+      sliderInput("slider_sepallength", "Set sepal length", min = 4.4, max = 7, value = 5), # to select a range of Sepal.Length values.
+      actionButton("button_filter", "Filter data") # to filter the data
     ),
     mainPanel(
-      tableOutput("iristable")
+      tableOutput("iris_table")
     )
   )
 )
 
 server <- function(input, output, session) {
-  data <- subset(iris, Species == input$species)
+  filtered_data <- reactive({
+    input$species
+    subset(iris, Species == input$select_species)
+  })
 
+  output$iris_table <- renderTable({ filtered_data() })
+  
+  
+  data2 <- eventReactive(input$button_filter, {
+    subset(filtered_data, Sepal.Length == input$slider_sepallength)
+    updateSliderInput(session, "slider_sepallength", 
+                      min = min(filtered_data()$Sepal.Length), 
+                      max = max(filtered_data()$Sepal.Length), 
+                      value = min(filtered_data()$Sepal.Length))
+  })
+  output$iris_table <- renderTable({ data2() })
+  
+  
+  observe({
+    print(paste("The user selected a different Species: ", input$select_species))
+  })
+  
 }
 
 shinyApp(ui, server, options = list(display.mode = "showcase"))
